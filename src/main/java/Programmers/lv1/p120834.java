d81cde46c9ba18943c0997f5858969c6141882b6
tkdtn4657 <tkdtn101226@gmail.com>
2023-04-15 21:34:33 +0900

    [PG] p120834

---
+++ src/main/java/Programmers/lv1/p120834.java
@@ -0,0 +1,15 @@
+package Programmers.lv1;
+
+public class p120834 {
+    public String solution(int age) {
+        String answer = new String(Integer.toString(age));
+        char[] c = new char[answer.length()];
+        char[] atoj = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
+
+        for(int i = 0; i < answer.length(); i++){
+            c[i] = atoj[Integer.parseInt(String.valueOf(answer.charAt(i)))];
+
+        }
+        return new String(c);
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import Programmers.lv1.*;
 
 public class Main {
     public static void main(String[] args) {
-        p120844 sol = new p120844();
-        System.out.println(sol.solution(new int[] {4, 455, 6, 4, -1, 45, 6}, "left"));
+        p120834 sol = new p120834();
+        System.out.println(sol.solution(23));
     }
 }
