59ef07b78b78d3850c6fe8754a077cb956bf43f2
tkdtn4657 <tkdtn101226@gmail.com>
2023-04-15 21:06:43 +0900

    [PG] p120905

---
+++ src/main/java/Programmers/lv1/p120905.java
@@ -0,0 +1,22 @@
+package Programmers.lv1;
+
+import java.util.ArrayList;
+
+public class p120905 {
+    public int[] solution(int n, int[] numlist) {
+
+        ArrayList<Integer> a = new ArrayList<>();
+
+        for(int i = 0; i < numlist.length; i++){
+            if(numlist[i] % n == 0){
+                a.add(numlist[i]);
+            }
+        }
+        int[] answer = new int[a.size()];
+
+        for(int i = 0; i < answer.length; i++){
+            answer[i] = a.get(i);
+        }
+        return answer;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,10 +3,11 @@ package org.example;
 import Programmers.lv1.p120845;
 import Programmers.lv1.p120862;
 import Programmers.lv1.p120895;
+import Programmers.lv1.p120905;
 
 public class Main {
     public static void main(String[] args) {
-        p120895 sol = new p120895();
-        System.out.println(sol.solution("hello", 1, 2));
+        p120905 sol = new p120905();
+        System.out.println(sol.solution(3, new int[] {4, 5, 6, 7, 8, 9, 10, 11, 12}));
     }
 }
