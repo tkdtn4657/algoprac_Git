7ec59cce3be3e207f56c34d7bc24a55e1a66dd6e
tkdtn4657 <tkdtn101226@gmail.com>
2023-04-15 10:15:14 +0900

    [PG] p120823

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import Programmers.lv1.p120845;
 
 public class Main {
     public static void main(String[] args) {
-        p120845 p120845 = new p120845();
-        System.out.println(p120845.solution(new int[] {10, 8, 6}, 3));
+//        p120823 p120823 = new p120823();
+//        System.out.println(p120823.solution());
     }
 }
