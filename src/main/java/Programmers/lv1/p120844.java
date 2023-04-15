9368770f63e6cc06f56517cae2680eba4b539eaf
tkdtn4657 <tkdtn101226@gmail.com>
2023-04-15 21:20:00 +0900

    [PG] p120844

---
+++ src/main/java/Programmers/lv1/p120844.java
@@ -0,0 +1,22 @@
+package Programmers.lv1;
+
+public class p120844 {
+    public int[] solution(int[] numbers, String direction) {
+        int temp;
+        if (direction.equals("right")) {
+            temp = numbers[numbers.length - 1];
+            for (int i = numbers.length - 1; i > 0; i--) {
+                numbers[i] = numbers[i - 1];
+            }
+            numbers[0] = temp;
+        } else if (direction.equals("left")) {
+            temp = numbers[0];
+            for (int i = 0; i < numbers.length - 1; i++) {
+                numbers[i] = numbers[i + 1];
+            }
+            numbers[numbers.length - 1] = temp;
+        }
+
+        return numbers;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,10 @@
 package org.example;
 
-import Programmers.lv1.p120845;
-import Programmers.lv1.p120862;
-import Programmers.lv1.p120895;
-import Programmers.lv1.p120905;
+import Programmers.lv1.*;
 
 public class Main {
     public static void main(String[] args) {
-        p120905 sol = new p120905();
-        System.out.println(sol.solution(3, new int[] {4, 5, 6, 7, 8, 9, 10, 11, 12}));
+        p120844 sol = new p120844();
+        System.out.println(sol.solution(new int[] {4, 455, 6, 4, -1, 45, 6}, "left"));
     }
 }
