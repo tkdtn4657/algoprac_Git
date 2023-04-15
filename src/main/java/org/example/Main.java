4f0f17941a30078d1323c29fdec6d5308dc6b725
tkdtn4657 <tkdtn101226@gmail.com>
2023-04-15 10:06:37 +0900

    init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import Programmers.lv1.p120845;
+
+public class Main {
+    public static void main(String[] args) {
+        p120845 p120845 = new p120845();
+        System.out.println(p120845.solution(new int[] {10, 8, 6}, 3));
+    }
+}
