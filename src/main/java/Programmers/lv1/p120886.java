9ee9279ded9d5732958d853ae995c229d2665654
tkdtn4657 <tkdtn101226@gmail.com>
2023-04-16 20:36:23 +0900

    [PG] p120886

---
+++ src/main/java/Programmers/lv1/p120886.java
@@ -0,0 +1,33 @@
+package Programmers.lv1;
+
+
+public class p120886 {
+    public int solution(String before, String after) {
+        int answer = 0;
+        int[] count1 = new int[26];
+        int[] count2 = new int[26];
+        for(int i = 0; i < before.length(); i++){
+            count1[(int)before.charAt(i) - 97]++;
+
+        }
+
+        for(int i = 0; i < after.length(); i++){
+            count2[(int)after.charAt(i) - 97]++;
+        }
+
+        if(equalArray(count1, count2)){
+            return 1;
+        }
+
+        return 0;
+    }
+
+    boolean equalArray(int[] a, int[] b){
+        for(int i = 0; i < a.length; i++){
+            if(a[i] != b[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import Programmers.lv1.*;
 
 public class Main {
     public static void main(String[] args) {
-        p120838 sol = new p120838();
-        System.out.println(sol.solution(".... . .-.. .-.. ---"));
+        p120886 sol = new p120886();
+        System.out.println(sol.solution("allpe", "apple"));
     }
 }
