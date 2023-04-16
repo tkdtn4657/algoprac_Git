8b2e9e36f7f6d27fbfeda22e6011b937c97b2639
tkdtn4657 <tkdtn101226@gmail.com>
2023-04-16 21:13:11 +0900

    [PG] p120887

---
+++ src/main/java/Programmers/lv1/p120887.java
@@ -0,0 +1,23 @@
+package Programmers.lv1;
+
+public class p120887 {
+    public int solution(int i, int j, int k) {
+        int answer = 0;
+
+        for(int start = i; start <= j; start++){
+            answer += comp(Integer.toString(start), k);
+        }
+
+        return answer;
+    }
+
+    int comp(String s, int k){
+        int count = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(Integer.parseInt(String.valueOf(s.charAt(i))) == k){
+                count++;
+            }
+        }
+        return count;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import Programmers.lv1.*;
 
 public class Main {
     public static void main(String[] args) {
-        p120842 sol = new p120842();
-        System.out.println(sol.solution(new int[] {1, 2, 3, 4, 5, 6, 7, 8}, 2));
+        p120887 sol = new p120887();
+        System.out.println(sol.solution(1, 13, 1));
     }
 }
