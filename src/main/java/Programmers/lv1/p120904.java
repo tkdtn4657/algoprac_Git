9e28c251816fbc796925218e4623690d3f33b9ce
tkdtn4657 <tkdtn101226@gmail.com>
2023-04-16 10:23:28 +0900

    [PG] p120904

---
+++ src/main/java/Programmers/lv1/p120904.java
@@ -0,0 +1,16 @@
+package Programmers.lv1;
+
+public class p120904 {
+    public int solution(int num, int k) {
+        int answer = 0;
+        String s = Integer.toString(num);
+
+        for(int i = 0; i < s.length(); i++){
+            if(Character.getNumericValue(s.charAt(i)) == k){
+                return i+1;
+            }
+        }
+
+        return -1;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import Programmers.lv1.*;
 
 public class Main {
     public static void main(String[] args) {
-        p120897 sol = new p120897();
-        System.out.println(sol.solution(29));
+        p120904 sol = new p120904();
+        System.out.println(sol.solution(29183, 1));
     }
 }
