bb44097a57711b586ef63f012f7f20a0d60acf76
tkdtn4657 <tkdtn101226@gmail.com>
2023-04-16 10:31:41 +0900

    [PG] p120846

---
+++ src/main/java/Programmers/lv1/p120846.java
@@ -0,0 +1,22 @@
+package Programmers.lv1;
+
+public class p120846 {
+    public int solution(int n) {
+        int answer = 0;
+        int count = 0;
+        for (int i = 0; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    count++;
+                }
+            }
+
+            if (count > 2){
+                answer++;
+            }
+                count = 0;
+        }
+
+        return answer;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import Programmers.lv1.*;
 
 public class Main {
     public static void main(String[] args) {
-        p120904 sol = new p120904();
-        System.out.println(sol.solution(29183, 1));
+        p120846 sol = new p120846();
+        System.out.println(sol.solution(10));
     }
 }
