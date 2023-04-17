7d203ff5eef6fb67a2328a5ff78d3a95a729918b
tkdtn4657 <tkdtn101226@gmail.com>
2023-04-17 16:51:57 +0900

    [PG] p120890 미완

---
+++ src/main/java/Programmers/lv1/p120890.java
@@ -0,0 +1,19 @@
+package Programmers.lv1;
+
+public class p120890 {
+
+    public int solution(int[] array, int n) {
+        int answer = 0;
+        int judgeNumber = n - array[0];
+        if(judgeNumber < 0) judgeNumber *= -1;
+        answer = array[0];
+        for(int i = 1; i < array.length; i++){
+            if(judgeNumber > n-array[i]){
+                //if(judgeNumber )
+                judgeNumber = n-array[i];
+                answer = array[i];
+            }
+        }
+        return answer;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,14 @@
 package org.example;
 
+import Baekjoon.p10026;
 import Programmers.lv1.*;
 
 public class Main {
     public static void main(String[] args) {
-        p120887 sol = new p120887();
-        System.out.println(sol.solution(1, 13, 1));
+//        p120890 sol = new p120890();   //[PG] 스타일
+//        System.out.println(sol.solution(new int[] {3, 10, 28}, 20));
+
+        p10026 solv = new p10026();//[BOJ] 스타일
+        solv.main();
     }
 }
