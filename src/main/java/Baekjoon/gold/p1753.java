00435c56162249f70659f45f5993faf96d4f3dde
tkdtn4657 <tkdtn101226@gmail.com>
2023-04-21 13:41:45 +0900

    백준p1753 코드따라쓰기

--- src/main/java/Baekjoon/gold/p1753.java
+++ src/main/java/Baekjoon/gold/p1753.java
@@ -1,6 +1,85 @@
 package Baekjoon.gold;
 
-public class p1753{ // gold4 최단경로
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
+class p1753 {
 
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+
+        ArrayList<Edge>[] e = new ArrayList[V];
+        int k = Integer.parseInt(br.readLine()) - 1;
+        boolean[] visit = new boolean[V];
+        int[] wei = new int[V];
+        for (int i = 0; i < V; i++) { // 초기화
+            wei[i] = Integer.MAX_VALUE;
+            e[i] = new ArrayList<Edge>();
+        }
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken()) - 1;
+            int v = Integer.parseInt(st.nextToken()) - 1;
+            int w = Integer.parseInt(st.nextToken());
+            e[u].add(new Edge(v, w));
+        }
+
+        PriorityQueue<Edge> q = new PriorityQueue<>();
+        q.add(new Edge(k, 0));
+        wei[k] = 0;
+
+        while (!q.isEmpty()) {
+            Edge now = q.poll();
+
+            if(visit[now.v]) continue;
+            visit[now.v] = true;
+            for (int i = 0; i < e[now.v].size(); i++) {
+                if(wei[e[now.v].get(i).v]>wei[now.v] + e[now.v].get(i).w) {
+                    wei[e[now.v].get(i).v] = wei[now.v] + e[now.v].get(i).w;
+                    q.offer(new Edge(e[now.v].get(i).v, wei[e[now.v].get(i).v]));
+                }
+            }
+        }
+
+        for(int i=0;i<V;i++) {
+            if(wei[i]==Integer.MAX_VALUE) {
+                sb.append("INF\n");
+            }else {
+                sb.append(wei[i]).append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
+
+    static class Edge implements Comparable<Edge>{
+        int v, w;
+
+        public Edge(int v, int w) {
+            super();
+            this.v = v;
+            this.w = w;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return this.w-o.w;
+        }
+    }
 }
