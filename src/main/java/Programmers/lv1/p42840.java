c2ed5e741fb230493ffe9e943df7d7480d7cf7ea
tkdtn4657 <tkdtn101226@gmail.com>
2023-04-26 11:41:01 +0900

    [PG] p42840 코드실행은 맞는데 제출했을 때 여러개가 실패함

--- src/main/java/Programmers/lv1/p42840.java
+++ src/main/java/Programmers/lv1/p42840.java
@@ -1,10 +1,92 @@
 package Programmers.lv1;
 
+import java.util.ArrayList;
+
 public class p42840 {
+
+    static ArrayList<Integer> result = new ArrayList<Integer>();
+
     public int[] solution(int[] answers) {
-        int[] answer = {};
+        int answerSize = answers.length;
+//        int[] supoOne = new int[answerSize];
+//        int[] supoTwo = new int[answerSize];
+//        int[] supoThree = new int[answerSize];
+        int[] highScore = new int[3];
+        ArrayList <Integer> result = new ArrayList<Integer>();
+
+        highScore[0] = supoOneResult(answerSize, answers);
+        highScore[1] = supoTwoResult(answerSize, answers);
+        highScore[2] = supoThreeResult(answerSize, answers);
+
+        int max = 0;
 
 
-        return answer;
+
+        if(highScore[0] > highScore[1]){
+            max = highScore[0];
+        }else{
+            max = highScore[1];
+        }if(max < highScore[2]){
+            max = highScore[2];
+        }
+
+        for(int i = 0; i < highScore.length; i++){
+            if(highScore[i] == max) {
+                result.add(highScore[i]);
+            }
+        }
+
+        int[] realResult = new int[result.size()];
+
+        for(int i = 0; i < result.size(); i++){
+            realResult[i] = i+1;
+        }
+
+        return realResult;
+    }
+
+    static int supoOneResult(int a, int[] answers) {
+        int[] aArray = new int[a];
+
+        int count = 0;
+
+        for (int i = 0; i < a; i++) {
+            aArray[i] = i % 5 + 1;
+
+            if (aArray[i] == answers[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    static int supoTwoResult(int b, int[] answers) { // 2, 1, 2, 3, 2, 4, 2, 5 | 2, 1, 2, 3, 2, 4, 2, 5
+        int[] bPattern = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] bArray = new int[b];
+
+        int count = 0;
+
+        for (int i = 0; i < b; i++) {
+            bArray[i] = bPattern[i % bPattern.length];
+            if (bArray[i] == answers[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    static int supoThreeResult(int c, int[] answers) { // 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 | 3, 3, 1, 1, 2, 2, 4, 4, 5, 5
+        int[] cPattern = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[] cArray = new int[c];
+
+        int count = 0;
+
+        for (int i = 0; i < c; i++) {
+            cArray[i] = cPattern[i % cPattern.length];
+            if (cArray[i] == answers[i]) {
+                count++;
+            }
+        }
+        return count;
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,17 +7,20 @@ import Baekjoon.gold.p4485;
 import Baekjoon.silver.p11724;
 import Baekjoon.silver.p1260;
 import Baekjoon.silver.p2178;
+import Programmers.lv0.*;
+import Programmers.lv1.*;
+import Programmers.lv2.*;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-//        p120890 sol = new p120890();   //[PG] 스타일
-//        System.out.println(sol.solution(new int[] {3, 10, 28}, 20));
+        p42840 sol = new p42840();   //[PG] 스타일
+        System.out.println(sol.solution(new int[] {1, 2, 3, 4, 5}));
 
 
-        p4485 solv = new p4485();//[BOJ] 스타일
-        solv.main();
+//        p4485 solv = new p4485();//[BOJ] 스타일
+//        solv.main();
 
 
     }
