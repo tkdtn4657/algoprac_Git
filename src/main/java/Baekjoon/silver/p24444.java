b7e66a1702c13e2598d4f06babde0426a7b678cc
tkdtn4657 <tkdtn101226@gmail.com>
2023-04-29 21:27:39 +0900

    [BOJ] p24444 값 입력 구현

--- src/main/java/Baekjoon/silver/p24444.java
+++ src/main/java/Baekjoon/silver/p24444.java
@@ -1,11 +1,46 @@
 package Baekjoon.silver;
 
+import javax.lang.model.type.ArrayType;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class p24444 {
 
-    public static void main(String[] args) {
+    static boolean[] isVisit;
+    static ArrayList<Integer>[] a;
 
+    public static void main() {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int r = sc.nextInt();
+        int u, v;
+
+
+        isVisit = new boolean[m+1];
+        a = new ArrayList[m+1];
+
+        for(int i = 1; i < m + 1; i++){
+            a[i] = new ArrayList<Integer>();
+        }
+
+        for(int i = 0; i < n; i++){
+            u = sc.nextInt();
+            v = sc.nextInt();
+
+            a[u].add(v);
+            a[v].add(u);
+
+        }
+
+        BFS(r);
+    }
+
+    static void BFS(int r){
 
 
     }
+
 
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,7 @@ import Baekjoon.gold.p4485;
 import Baekjoon.silver.p11724;
 import Baekjoon.silver.p1260;
 import Baekjoon.silver.p2178;
+import Baekjoon.silver.p24444;
 import Programmers.lv0.*;
 import Programmers.lv1.*;
 import Programmers.lv2.*;
@@ -15,12 +16,12 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        p42840 sol = new p42840();   //[PG] 스타일
-        System.out.println(sol.solution(new int[] {1, 2, 3, 4, 5}));
+//        p42840 sol = new p42840();   //[PG] 스타일
+//        System.out.println(sol.solution(new int[] {1, 2, 3, 4, 5}));
 
 
-//        p4485 solv = new p4485();//[BOJ] 스타일
-//        solv.main();
+        p24444 solv = new p24444();//[BOJ] 스타일
+        solv.main();
 
 
     }
