f8aa6f2e102a4764f7ac13b077c22e0f21742e48
tkdtn4657 <tkdtn101226@gmail.com>
2023-04-30 12:23:15 +0900

    [BOJ] p24444 로직 이해하고 직접 짜서 통과git add .git add .git add . 범위설정 좀 헷갈렸는데 이거 풀면서 좀 명확해진듯

--- src/main/java/Baekjoon/silver/p24444.java
+++ src/main/java/Baekjoon/silver/p24444.java
@@ -1,5 +1,8 @@
 package Baekjoon.silver;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class p24444 {
@@ -7,44 +10,48 @@ public class p24444 {
     static boolean[] isVisit;
     static ArrayList<Integer>[] a;
 
-    public static void main() {
-        Scanner sc = new Scanner(System.in);
+    public static void main() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int r = sc.nextInt();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+
         int u, v;
 
+        isVisit = new boolean[n+1];
+        a = new ArrayList[n+1];
 
-        isVisit = new boolean[m+1];
-        a = new ArrayList[m+1];
-
-        for(int i = 1; i < m + 1; i++){
+        for(int i = 1; i < n + 1; i++){
             a[i] = new ArrayList<Integer>();
         }
 
-        for(int i = 0; i < n; i++){
-            u = sc.nextInt();
-            v = sc.nextInt();
+        for(int i = 0; i < m; i++){
+            st = new StringTokenizer(br.readLine());
+            u = Integer.parseInt(st.nextToken());
+            v = Integer.parseInt(st.nextToken());
 
             a[u].add(v);
             a[v].add(u);
-
         }
 
-        for(int i = 1; i < m + 1; i++){
+        for(int i = 1; i < n + 1; i++){
             if(!a[i].isEmpty()) {
                 Collections.sort(a[i]);
             }
         }
-        BFS(r, m);
+
+        BFS(r, n, m);
+
     }
 
-    static void BFS(int r, int m){ // r이 1이라고 가정했을 때
+    static void BFS(int r, int n, int m){ // r이 1이라고 가정했을 때
         Queue<Integer> queue = new LinkedList<>();
 
         int temp;
-        int[] count = new int[m+1];
+        int[] count = new int[n+1];
         int countNum = 1;
 
         isVisit[r] = true;
@@ -62,7 +69,7 @@ public class p24444 {
             }
         }
 
-        for(int i = 1; i < m+1; i++){
+        for(int i = 1; i < n + 1; i++){
             System.out.println(count[i]);
         }
     }
