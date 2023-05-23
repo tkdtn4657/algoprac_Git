5357ef9c330c198eecf2dd35bdcb4068d6193ec2
tkdtn4657 <tkdtn101226@gmail.com>
2023-05-24 00:00:58 +0900

    ArrayList에서 HashMap으로 변경 후 중복값 입력방지를 통해 메모리초과를 피함 속도 매우느리지만 통과

--- src/main/java/Baekjoon/silver/p1418.java
+++ src/main/java/Baekjoon/silver/p1418.java
@@ -3,7 +3,6 @@ package Baekjoon.silver;
 import java.util.*;
 import java.io.*;
 
-import static java.lang.Math.sqrt;
 
 public class p1418 {
     public static void main() throws IOException {
@@ -11,32 +10,59 @@ public class p1418 {
 
         int n = Integer.parseInt(br.readLine());
         int k = Integer.parseInt(br.readLine());
-        int[] a = new int[n+1];
-        ArrayList<Integer> maxArray = new ArrayList<Integer>();
-        //int i = 2;
+        int[] a = new int[n + 1];
+        HashMap<Integer, Integer> hs = new HashMap<Integer, Integer>();
+
+
         for(int i = 0; i <= n; i++) {
             a[i] = i;
+            hs.put(i, 0);
         } // 1 2 3 4 5 6 7 8 9 10
-
         //1은 무조건 됨
         for(int i = 2; i <= n; i++){
             int soinsu = a[i];
             for(int j = 2; j <= soinsu; j++) {//sqrt(soinsu)?
                 while(soinsu % j == 0) {
-                    maxArray.add(j);
                     soinsu /= j;
+                    hs.put(i, j);//하다보면 맥스
                 }
-                a[i] = Collections.max(maxArray);
-                maxArray = new ArrayList<>();
-                maxArray.add(1);
             }
         }
         int count = 0;
         for(int i = 1; i < a.length; i++) {
-            if(a[i] <= k){
+            if(hs.get(i) <= k){
                 count++;
             }
         }
         System.out.println(count);
     }
 }
+
+/* //다른사람풀이 속도겁나빠름..
+* import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int k = Integer.parseInt(br.readLine());
+        int[] arr = new int[100001];
+        for(int i = 2; i <= n; i++) {
+            if(arr[i] != 0)
+                continue;
+            for(int j = i; j <= n; j += i) {
+                arr[j] = i;
+            }
+        }
+        int cnt = 0;
+        for(int i = 1; i <= n; i++) {
+            if(arr[i] <= k)
+                cnt++;
+        }
+        System.out.println(cnt);
+    }
+}
+*
+*
+* */
