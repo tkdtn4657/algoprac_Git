28919766e2f480dcd15876b566e5e7a4343483ce
tkdtn4657 <tkdtn101226@gmail.com>
2023-06-02 21:03:27 +0900

    [BOJ] p1475 구현

--- src/main/java/Baekjoon/silver/p1475.java
+++ src/main/java/Baekjoon/silver/p1475.java
@@ -1,36 +1,79 @@
 package Baekjoon.silver;
 
 import java.io.*;
+import java.util.*;
 
-public class p1475 { // 아직미완성
-    public static void main() throws IOException{
+public class p1475{
+    public static void main()throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        boolean[] b = new boolean[10];
-        int n = Integer.parseInt(br.readLine());
-        int one;
-        int countSet = 1;
-        
-        while(true){
-            one = n % 10;
+        StringBuilder n = new StringBuilder(br.readLine());
 
-            if(!b[one]){
-                b[one] = true;
-            }else if(one == 6 && !b[9]){
-                b[9] = true;
-            }else if(one == 9 && !b[6]){
-                b[6] = true;
-            }else {
-                countSet++;
-                b = new boolean[10];
-                b[one] = true;
-            }
-            if(n < 10){
-                break;
-            }
-            n /= 10;
+        int[] a = new int[10];
+
+        int length = n.length();
+        while (length-- > 0) {
+            int getNumber = n.charAt(0) - '0';
+            n.deleteCharAt(0);
+            a[getNumber]++;
         }
-        System.out.println(countSet);
 
+
+
+
+        int k = (a[6] + a[9]);
+        if (k % 2 == 0) {
+            a[6] = k / 2;
+            a[9] = k / 2;
+        }
+        else {
+            a[6] = k / 2 + 1;
+            a[9] = k / 2 + 1;
+        }
+
+        int max = 0;
+        for (int i : a) {
+            max = Math.max(max, i);
+        }
+
+        System.out.println(max);
     }
-
 }
+//public class p1475 { // 아직미완성
+//
+//    static int setCount = 1;
+//
+//    public static void main() throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringBuilder n = new StringBuilder(br.readLine());
+//
+//        int[] a = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+//        int[] numSet = a.clone();
+//
+//        int length = n.length();
+//        int getNumber;
+//        while (length-- > 0) {
+//            //index가 됨
+//            getNumber = n.charAt(0) - '0';
+//            n.deleteCharAt(0);
+//            if (a[getNumber] > 0) {
+//                a[getNumber]--;
+//            } else if (getNumber == 9 && a[6] > 0) {
+//                a[6]--;
+//            } else if (getNumber == 6 && a[9] > 0) {
+//                a[9]--;
+//            } else {
+//                plusSet(a, numSet);
+//                a[getNumber]--;
+//            }
+//        }
+//        System.out.println(setCount);
+//    }
+//
+//    static void plusSet(int[] a, int[] b) {
+//        for (int i = 0; i < a.length; i++) {
+//            a[i] += b[i];
+//        }
+//        setCount++;
+//    }
+//
+//}
