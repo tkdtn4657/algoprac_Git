d94d8a2d723e1dfd3448ec2a0fc6fcc115aa2a3b
tkdtn4657 <tkdtn101226@gmail.com>
2023-06-07 07:09:12 +0900

    p19532 역행렬방식으로 풀ㅇ보기

--- src/main/java/Baekjoon/bronze/p19532.java
+++ src/main/java/Baekjoon/bronze/p19532.java
@@ -13,15 +13,18 @@ public class p19532 {
         for(int i = 0; i < 6; i++){
             a[i] = Integer.parseInt(st.nextToken());
         }
+        //a[0] = a | a[1] = b | a[2] = c | a[3] = d | a[4] = e | a[5] = f
+        bw.write((a[2]*a[4] - a[1]*a[5])/(a[0]*a[4] - a[1]*a[3]) + " "
+                + (a[2]*a[3] - a[0]*a[5])/(a[1]*a[3] - a[0]*a[4]));
 
-        for(int i = -999; i < 1000; i++){
-            for(int j = -999; j < 1000; j++){
-                if(a[0]*i + a[1]*j == a[2] && a[3]*i + a[4]*j == a[5]){
-                        bw.write(i + " " + j);
-                        break;
-                }
-            }
-        }
+        //ae -bd 이게 0이 아니면 해를 구할 수 있음 또 이게 분모가 됨bd - ae
+        //cd - fa, ce - fb 이 두개 그니까 x와 y의 답을 찾는 과정
+        //ca - fb, cd - fe
+
+        //ce - bf, cd - af 분자
+        //ae - bd, bd - ae 분모
+
+
         bw.flush();
         bw.close();
         br.close();
