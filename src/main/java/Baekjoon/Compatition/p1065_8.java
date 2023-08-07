71f98e652ae29e55dfdb31db4424fe80b1f535e7
tkdtn4657 <tkdtn101226@gmail.com>
2023-08-07 11:55:54 +0900

    [BOJ] GrandArena 대회1 문제H번 각 행과 열의 배열을 따로만들어서 계산 구현

--- src/main/java/Baekjoon/Compatition/p1065_8.java
+++ src/main/java/Baekjoon/Compatition/p1065_8.java
@@ -6,46 +6,41 @@ import java.util.*;
 public class p1065_8 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         int q = Integer.parseInt(st.nextToken());
 
-        int[][] a = new int[n][m];
+//        int[][] a = new int[n][m];
+        int[] row = new int[m];
+        int[] col = new int[n];
 
         int mode;
         int line;
         int v;
-        StringBuilder sb;
+
         for(int i = 0; i < q; i++){
-            st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine(), " ");
             mode = Integer.parseInt(st.nextToken());
             line = Integer.parseInt(st.nextToken());
             v = Integer.parseInt(st.nextToken());
 
             if(mode == 1){
                 //1일때는 row
-                for(int j = 0; j < m; j++) {
-                    a[line-1][j] += v;
-                }
-            }else if(mode == 2){
+                col[line-1] += v;
+            }else{
                 //2일때는 col
-                for(int j = 0; j < n; j++) {
-                    a[j][line-1] += v;
-                }
+
+                row[line-1] += v;
             }
         }
-
-        sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                sb.append(a[i][j]).append(" ");
+                sb.append(col[i] + row[j]).append(" ");
             }
             sb.append("\n");
         }
-
         System.out.println(sb);
-
     }
 }
