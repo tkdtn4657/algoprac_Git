86b956872b15271e744d680366b6e3b49c35abd0
tkdtn4657 <tkdtn101226@gmail.com>
2023-08-28 11:52:23 +0900

    [BOJ] p11050 파스칼삼각형 형식으로 dp배열 활용해서 이항계수문제 해결

--- src/main/java/Baekjoon/bronze/p11050.java
+++ src/main/java/Baekjoon/bronze/p11050.java
@@ -1,19 +1,34 @@
 package Baekjoon.bronze;
 
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-public class p11050 {
+class p11050 {
 
-    public static void main() {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = sc.nextInt();
-        int k = sc.nextInt();
+        StringTokenizer st =new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
-        
+        int[][] dp = new int[n+1][n+1];
 
+        //파스칼 삼각형 뼈대
+        for(int i = 0; i <= n; i++){
+            dp[i][1] = i;
+            dp[i][0] = 1;
+            dp[i][i] = 1;
+        }
 
+        //5C2 == 4C1 + 4C3
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                dp[i][j] = dp[i-1][j] + dp[i-1][j-1];
+            }
+        }
+
+        System.out.println(dp[n][k]);
 
     }
-
 }
