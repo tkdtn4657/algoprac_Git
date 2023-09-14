da452363edb13e8913cd69c4723f5c55fa6072a0
tkdtn4657 <tkdtn101226@gmail.com>
2023-09-14 12:00:35 +0900

    [BOJ] p11726 dp구현

---
+++ src/main/java/Baekjoon/silver/p11726.java
@@ -0,0 +1,24 @@
+package Baekjoon.silver;
+
+import java.io.*;
+import java.util.*;
+
+public class p11726 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n+1];
+
+        //2x1의 타일은 하나만가능
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i = 3; i <= n; i++){
+            dp[i] = (dp[i-1] + dp[i-2]) % 10007;
+        }
+
+        System.out.println(dp[n]);
+
+    }
+}
