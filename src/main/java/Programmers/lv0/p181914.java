24af821914d343a69be390fe03775030dc431c9f
tkdtn4657 <tkdtn101226@gmail.com>
2023-09-19 01:02:00 +0900

    [Programmers] lv0 9로 나눈 나머지 문제 Stream풀이 추가

--- src/main/java/Programmers/lv0/p181914.java
+++ src/main/java/Programmers/lv0/p181914.java
@@ -1,11 +1,14 @@
 package Programmers.lv0;
 
+import java.util.Arrays;
 import java.util.function.Function;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class p181914 {
     public static void main(String[] args) {
 
-        System.out.println(new Solution().solution("13"));
+        System.out.println(new Solution().solution2("78720646226947352489"));
 
     }
 }
@@ -23,5 +26,29 @@ class Solution {
     };
 
         return counter.apply(number);
+    }
+
+    public int solution2(String number) {
+        //mapToInt는 10만의 제한때문에 할 수 없음
+//        return IntStream.range(0, number.toCharArray().length)
+//                .mapToObj(i -> Character.getNumericValue(number.charAt(i)))
+//                .reduce(0, Integer::sum) % 9;
+
+
+//        number.chars()
+//                .map(Character::getNumericValue)
+//                .reduce(0, Integer::sum);
+//        return sum % 9;
+
+//        return number.chars()
+//                .map(ch -> Character.getNumericValue(ch))
+//                .reduce(0, (x, y) -> x + y) % 9;
+
+
+
+        return number.chars()
+                .map(Character::getNumericValue)
+                .reduce(0,(x, y) -> x + y) % 9;
+
     }
 }
