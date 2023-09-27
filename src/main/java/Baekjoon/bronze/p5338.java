3cb1f65732a26a4c8fc3831349be8e7b325546d5
tkdtn4657 <tkdtn101226@gmail.com>
2023-09-27 16:24:12 +0900

    [BOJ] p5338 구현

---
+++ src/main/java/Baekjoon/bronze/p5338.java
@@ -0,0 +1,14 @@
+package Baekjoon.bronze;
+
+import java.io.*;
+import java.util.*;
+
+public class p5338 {
+    public static void main(String[] args) {
+        System.out.println("       _.-;;-._\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-;;-._|\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-''-._|");
+    }
+}
