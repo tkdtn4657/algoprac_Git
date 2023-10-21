c4ecbb57b8ca1ee4baede79649c58bac444a21d2
tkdtn4657 <tkdtn101226@gmail.com>
2023-10-21 17:57:02 +0900

    [BOJ] p9012 괄호 스택활용 구현

---
+++ src/main/java/Baekjoon/silver/p9012.java
@@ -0,0 +1,40 @@
+package Baekjoon.silver;
+
+import java.util.*;
+import java.io.*;
+
+public class p9012 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        Stack<Character> stack;
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < n; i++){
+
+            String s = br.readLine();
+            stack = new Stack<>();
+            boolean flag = true;
+
+            for(int j = 0; j < s.length(); j++){
+                switch (s.charAt(j)){
+                    case '(':
+                        stack.push('(');
+                        break;
+                    case')':
+                        if(!stack.isEmpty()){
+                            stack.pop();
+                        }else{
+                            flag = false;
+                        }
+                }
+            }
+            if(!stack.isEmpty() || !flag){
+                sb.append("NO").append("\n");
+            } else{
+                sb.append("YES").append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
+}
