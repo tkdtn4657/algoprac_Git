875c45354db6ec449a08556062d9866ce33d8620
tkdtn4657 <tkdtn101226@gmail.com>
2023-12-25 22:48:38 +0900

    [BOJ] p24078 구현

---
+++ src/main/java/Baekjoon/bronze/p24078.java
@@ -0,0 +1,14 @@
+package Baekjoon.bronze;
+
+import java.io.*;
+import java.util.*;
+
+public class p24078 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(n%21);
+
+    }
+}
