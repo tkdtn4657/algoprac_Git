248d7053bc5fe6644a952033dc039daea305398a
tkdtn4657 <tkdtn101226@gmail.com>
2023-12-29 13:57:10 +0900

    [BOJ] p5525 IOIOI 문자열 구현 100점

--- src/main/java/Baekjoon/silver/p5525.java
+++ src/main/java/Baekjoon/silver/p5525.java
@@ -3,35 +3,32 @@ package Baekjoon.silver;
 import java.io.*;
 
 public class p5525 {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
 
-        StringBuilder s = new StringBuilder(br.readLine());
-
-        StringBuilder temp = new StringBuilder("I");
-        for(int i = 0; i < n; i++) {
-            temp.append("OI");
-        }
+        String s = br.readLine();
 
         int count = 0;
-        for(int start = 0, end = temp.length(); end <= m;){
-            if(builderSubString(start++,end++,s, temp)){
+        int answer = 0;
+        int oPoint = 1;
+
+        while (oPoint < s.length()-1) {
+            if (s.charAt(oPoint - 1) == 'I' && s.charAt(oPoint) == 'O' && s.charAt(oPoint + 1) == 'I') {
                 count++;
+                if (count == n) {
+                    count--;
+                    answer++;
+                }
+                oPoint++;
+            } else {
+                count = 0;
             }
+            oPoint++;
         }
 
-        System.out.println(count);
-    }
-
-    static boolean builderSubString(int start, int end, StringBuilder sb1, StringBuilder sb2){
-        for(int i = start; i < end; i++) {
-            if(sb1.charAt(i) != sb2.charAt(i-start)){
-                return false;
-            }
-        }
-        return true;
+        System.out.println(answer);
     }
 }
